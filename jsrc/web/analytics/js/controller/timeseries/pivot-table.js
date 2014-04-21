f368fccdc87b04a80923edaec5a55e4ebe2cecee
danfjian <daniel.jiang@oracle.com>
2014-04-21 13:19:54 +0800

    enable dimension reorganization after dragging the pivot header.

--- jsrc/web/analytics/js/controller/timeseries/pivot-table.js
+++ jsrc/web/analytics/js/controller/timeseries/pivot-table.js
@@ -12,163 +12,200 @@ define(['ojs/ojcore', 'knockout', 'jquery', '/analytics/js/common/ita-core.js',
         name: 'pivot-table',
         init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 
+            var pivotTemplate;
+
             function drawDim(xDims, yDims) {
-                $.get('/analytics/html/timeseries/pivot-table.html', function(resp) {
-                    var $pivotTable = $(resp);
-                    $(element).append($pivotTable);
-                    var xDimsWithComputedSpan = computeSpan(xDims);
-                    var yDimsWithComputedSpan = computeSpan(yDims);
+                $(element).empty();
+                var $pivotTable = $(pivotTemplate);
+                $(element).append($pivotTable);
+                
+                var xDimsWithComputedSpan = computeSpan(xDims);
+                var yDimsWithComputedSpan = computeSpan(yDims);
 
-                    var horizontalPaths = cartesianAttrPaths(xDims);
-                    var verticalPaths = cartesianAttrPaths(yDims);
-                    console.log(horizontalPaths);
-                    console.log(verticalPaths);
+                var horizontalPaths = cartesianAttrPaths(xDims);
+                var verticalPaths = cartesianAttrPaths(yDims);
 
-                    var horizontalHeader = toHorizontalHeader(xDimsWithComputedSpan);
-                    var verticalHeader = toVerticalHeader(yDimsWithComputedSpan);
-                    ko.applyBindings({
-                        xDims: xDims,
-                        yDims: yDims,
-                        horizontalPaths: horizontalPaths,
-                        verticalPaths: verticalPaths,
-                        horizontalHeader: horizontalHeader,
-                        verticalHeader: verticalHeader,
-                        showPaths: showPaths,
-                        showHorizontalDim: showHorizontalDim,
-                        showVerticalDim: showVerticalDim,
-                        handleHorizontalDrop: handleHorizontalDrop,
-                        handleVerticalDrop: handleVerticalDrop,
-                        // just for test
-                        "series": [{name: 'Series 1', items: [110, 55, 36, 22, 32, 39]}, {name: 'Series 2', items: [10, 155, 136, 122, 132, 139]}],
-                        "groups": [new Date(2014, 2, 4), new Date(2014, 2, 11), new Date(2014, 2, 18), new Date(2014, 2, 25), new Date(2014, 3, 1), new Date(2014, 3, 8)]
-                    }, $pivotTable[0]);
+                var horizontalHeader = toHorizontalHeader(xDimsWithComputedSpan);
+                var verticalHeader = toVerticalHeader(yDimsWithComputedSpan);
+                ko.applyBindings({
+                    xDims: xDims,
+                    yDims: yDims,
+                    horizontalPaths: horizontalPaths,
+                    verticalPaths: verticalPaths,
+                    horizontalHeader: horizontalHeader,
+                    verticalHeader: verticalHeader,
+                    showPaths: showPaths,
+                    showHorizontalDim: showHorizontalDim,
+                    showVerticalDim: showVerticalDim,
+                    handleHorizontalDrop: handleHorizontalDrop,
+                    handleVerticalDrop: handleVerticalDrop,
+                    // just for test
+                    "series": [{name: 'Series 1', items: [110, 55, 36, 22, 32, 39]}, {name: 'Series 2', items: [10, 155, 136, 122, 132, 139]}],
+                    "groups": [new Date(2014, 2, 4), new Date(2014, 2, 11), new Date(2014, 2, 18), new Date(2014, 2, 25), new Date(2014, 3, 1), new Date(2014, 3, 8)]
+                }, $pivotTable[0]);
 
 
-                    // just for showing the dimension path of a cell.
-                    function showPaths(x, y) {
-                        var hPath = horizontalPaths[x];
-                        var yPath = verticalPaths[y];
-                        var tips = "";
-                        $.each(hPath, function(i, step) {
-                            tips += '-->' + step.text;
-                        });
-                        tips += "<br/>";
-                        $.each(yPath, function(i, step) {
-                            tips += '-->' + step.text;
-                        });
-                        $("#pathsConsole").html(tips);
-                    }
-
-                    var isDragging = false;
-                    $('th').resizable();
-
-                    var $draggableHeader = $("#draggableHeader");
-                    var $dropLine = $("#dropLine");
-                    var $pivotCorner = $(".pivot-corner");
-
-                    $draggableHeader.draggable({
-                        cursorAt: { top: 0, left: 0 },
-                        cursor: 'move',
-                        revert: true,
-                        start: function() {
-                            isDragging = true;
-                        },
-                        stop: function() {
-                            isDragging = false;
-                            $dropLine.hide();
-                        }
+                // just for showing the dimension path of a cell.
+                function showPaths(x, y) {
+                    var hPath = horizontalPaths[x];
+                    var yPath = verticalPaths[y];
+                    var tips = "";
+                    $.each(hPath, function(i, step) {
+                        tips += '-->' + step.text;
                     });
+                    tips += "<br/>";
+                    $.each(yPath, function(i, step) {
+                        tips += '-->' + step.text;
+                    });
+                    $("#pathsConsole").html(tips);
+                }
 
-                    function showHorizontalDim(obj, event) {
-                        if (!isDragging) {
-                            var $containerTr = $(event.currentTarget).parent();
-                            var $thsInLine = $containerTr.find('th:not(.pivot-corner)');
+                var isDragging = false;
+                $('th').resizable();
 
-                            var pos = $thsInLine.offset();
-                            var height = $containerTr.height();
-                            var width = $(".pivot-vertical-header+:not(th)").prev().outerWidth();
+                var $draggableHeader = $("#draggableHeader");
+                var $dropLine = $("#dropLine");
+                var $pivotCorner = $(".pivot-corner");
 
-                            $draggableHeader.text(obj.dim.text);
-                            $draggableHeader.css('top', pos.top + 'px').css('left', pos.left - width + 'px')
-                                    .width(width).height(height).show();
-                        }
-                    }
+                var dragFromDim;
+                var dragFromX;
+                var dropToDim;
+                var dropToX;
+                var beforeTargetDim;
 
-                    function showVerticalDim(obj, event) {
-                        if (!isDragging) {
-                            var $sender = $(event.currentTarget);
-
-                            var $lastHorizontalHeader = $(".pivot-horizontal-header").parent(":last");
-                            var top = $lastHorizontalHeader.offset().top;
-                            var left = $sender.offset().left;
-                            var width = $sender.outerWidth();
-                            var height = $lastHorizontalHeader.outerHeight();
-
-                            $draggableHeader.text(obj.dim.text);
-                            $draggableHeader.css('top', top + 'px').css('left', left + 'px')
-                                    .width(width).height(height).show();
-                        }
-                    }
-
-                    function handleHorizontalDrop(obj, event) {
-                        if (isDragging) {
-                            var $sender = $(event.currentTarget);
-
-                            var currentDim = obj.dim;
-                            var currentDimIndex = findDimIndex(xDims, currentDim);
-
-                            var offsetY = event.offsetY;
-                            var dir = (offsetY < $sender.outerHeight() / 2) ? "before" : "after";
-
-                            var referredTr = $(".pivot-horizontal-header").parent().eq(currentDimIndex);
-                            var top = referredTr.offset().top;
-                            var left = $pivotCorner.outerWidth();
-                            if (dir === 'after') {
-                                top += referredTr.outerHeight();
-                            }
-
-                            $dropLine.width(referredTr.outerWidth() - left).height(5);
-                            $dropLine.css('top', top + 'px').css('left', left + 'px');
-                            $dropLine.addClass('h-line').removeClass('v-line').show();
-                        }
-                    }
-
-                    function handleVerticalDrop(obj, event) {
-                        if (isDragging) {
-                            var $sender = $(event.currentTarget);
-
-                            var currentDim = obj.dim;
-                            var currentDimIndex = findDimIndex(yDims, currentDim);
-
-                            console.log(event);
-                            var offsetX = event.offsetX;
-                            var dir = (offsetX < $sender.outerWidth() / 2) ? "before" : "after";
-                            console.log(offsetX);
-
-                            var top = $pivotCorner.outerHeight();
-                            var left = $sender.offset().left;
-                            if (dir === 'after') {
-                                left += $sender.outerWidth();
-                            }
-                            var height = $('.pivot-table').height() - top;
-
-                            $dropLine.height(height).width(5);
-                            $dropLine.css('top', top + 'px').css('left', left + 'px');
-                            $dropLine.addClass('v-line').removeClass('h-line').show();
-                        }
-                    }
-
-                    function findDimIndex(dims, dimToFind) {
-                        var index = -1;
-                        $.each(dims, function(i, dim) {
-                            if (dim.key === dimToFind.key) {
-                                index = i;
-                                return false;
-                            }
-                        });
-                        return index;
+                $draggableHeader.draggable({
+                    cursorAt: {top: -5, left: -5},
+                    cursor: 'move',
+                    revert: true,
+                    start: function() {
+                        isDragging = true;
+                    },
+                    stop: function() {
+                        isDragging = false;
+                        $dropLine.hide();
+                        reorganizeDims();
+                        drawDim(xDims, yDims);
                     }
                 });
+
+                function showHorizontalDim(obj, event) {
+                    if (!isDragging) {
+                        var $containerTr = $(event.currentTarget).parent();
+                        var $thsInLine = $containerTr.find('th:not(.pivot-corner)');
+
+                        var pos = $thsInLine.offset();
+                        var height = $containerTr.height();
+                        var width = $(".pivot-vertical-header+:not(th)").prev().outerWidth();
+
+                        dragFromDim = obj.dim;
+                        dragFromX = true;
+                        $draggableHeader.text(dragFromDim.text);
+                        $draggableHeader.css('top', pos.top + 'px').css('left', pos.left - width + 'px')
+                                .width(width).height(height).show();
+                    }
+                }
+
+                function showVerticalDim(obj, event) {
+                    if (!isDragging) {
+                        var $sender = $(event.currentTarget);
+
+                        var $lastHorizontalHeader = $(".pivot-horizontal-header").parent(":last");
+                        var top = $lastHorizontalHeader.offset().top;
+                        var left = $sender.offset().left;
+                        var width = $sender.outerWidth();
+                        var height = $lastHorizontalHeader.outerHeight();
+
+                        dragFromDim = obj.dim;
+                        dragFromX = false;
+                        $draggableHeader.text(dragFromDim.text);
+                        $draggableHeader.css('top', top + 'px').css('left', left + 'px')
+                                .width(width).height(height).show();
+                    }
+                }
+
+                function handleHorizontalDrop(obj, event) {
+                    if (isDragging) {
+                        var $sender = $(event.currentTarget);
+
+                        var offsetY = event.offsetY;
+                        beforeTargetDim = (offsetY < $sender.outerHeight() / 2);
+
+                        var top = $sender.offset().top;
+                        var left = $pivotCorner.outerWidth();
+                        if (!beforeTargetDim) {
+                            top += $sender.outerHeight();
+                        }
+                        var width = $('.pivot-table').outerWidth() - left;
+
+                        $dropLine.width(width).height(5);
+                        $dropLine.css('top', top + 'px').css('left', left + 'px');
+                        $dropLine.addClass('h-line').removeClass('v-line').show();
+
+                        dropToDim = obj.dim;
+                        dropToX = true;
+                    }
+                }
+
+                function handleVerticalDrop(obj, event) {
+                    if (isDragging) {
+                        var $sender = $(event.currentTarget);
+
+                        var offsetX = event.offsetX;
+                        beforeTargetDim = (offsetX < $sender.outerWidth() / 2);
+
+                        var top = $pivotCorner.outerHeight();
+                        var left = $sender.offset().left;
+                        if (!beforeTargetDim) {
+                            left += $sender.outerWidth();
+                        }
+                        var height = $('.pivot-table').height() - top;
+
+                        $dropLine.height(height).width(5);
+                        $dropLine.css('top', top + 'px').css('left', left + 'px');
+                        $dropLine.addClass('v-line').removeClass('h-line').show();
+
+                        dropToDim = obj.dim;
+                        dropToX = false;
+                    }
+                }
+
+                function findDimIndex(dims, dimToFind) {
+                    var index = -1;
+                    $.each(dims, function(i, dim) {
+                        if (dim.key === dimToFind.key) {
+                            index = i;
+                            return false;
+                        }
+                    });
+                    return index;
+                }
+
+                function reorganizeDims() {
+                    console.log(xDims);
+                    console.log(yDims);
+
+                    var dimToMove = null;
+                    if (dragFromX && dragFromDim) {
+                        var dimIndex = findDimIndex(xDims, dragFromDim);
+                        dimToMove = xDims.splice(dimIndex, 1)[0];
+                    } else if (!dragFromX && dragFromDim) {
+                        var dimIndex = findDimIndex(yDims, dragFromDim);
+                        dimToMove = yDims.splice(dimIndex, 1)[0];
+                    }
+
+                    if (dropToX) {
+                        var dimIndex = findDimIndex(xDims, dropToDim);
+                        !beforeTargetDim && dimIndex++;
+                        xDims.splice(dimIndex, 0, dimToMove);
+                    } else {
+                        var dimIndex = findDimIndex(yDims, dropToDim);
+                        !beforeTargetDim && dimIndex++;
+                        yDims.splice(dimIndex, 0, dimToMove);
+                    }
+
+                    console.log(xDims);
+                    console.log(yDims);
+                }
             }
 
             /**
@@ -185,6 +222,8 @@ define(['ojs/ojcore', 'knockout', 'jquery', '/analytics/js/common/ita-core.js',
                     attrsRepeat *= dim.attrs.length;
                 }
 
+
+                dim.span = 0;
                 var spanProduct = dims[dims.length - 1].attrs.length;
                 for (var i = dims.length - 2; i >= 0; i--) {
                     var dim = dims[i];
@@ -312,8 +351,11 @@ define(['ojs/ojcore', 'knockout', 'jquery', '/analytics/js/common/ita-core.js',
                 }
             }
 
-            $.getJSON('/samples/data/pivot-dimension.json', function(resp) {
-                drawDim(resp.x, resp.y);
+            $.getJSON('/samples/data/pivot-dimension.json', function(pivotData) {
+                $.get('/analytics/html/timeseries/pivot-table.html', function(pivotHtml) {
+                    pivotTemplate = pivotHtml;
+                    drawDim(pivotData.x, pivotData.y);
+                });
             });
         }
     });
